3d36124a07d688406fdcb76defdc548ed505f6f9
Martin Kihungi <mwangim4786@gmail.com>
2024-08-29 18:49:50 -0700

    0-stats.py 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+""" 0. Log parsing - Write a script that reads stdin line by line and computes metrics: """
 
 import sys
 
 
-i = 0
+line_count = 0
 sum_file_size = 0
 status_code = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
@@ -17,15 +17,15 @@ try:
             if status_line in status_code:
                 status_code[status_line] += 1
             sum_file_size += int(file_size)
-            i += 1
-            if i == 10:
+            line_count += 1
+            if line_count == 10:
                 print('File size: {:d}'.format(sum_file_size))
                 sorted_keys = sorted(status_code.keys())
                 for key in sorted_keys:
                     value = status_code[key]
                     if value != 0:
                         print('{}: {}'.format(key, value))
-                i = 0
+                line_count = 0
 except Exception:
     pass
 finally:
@@ -35,3 +35,4 @@ finally:
         value = status_code[key]
         if value != 0:
             print('{}: {}'.format(key, value))
+
